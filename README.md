37fb94e2cb572af4557eb6babfc76041a78b3ab1
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-11 17:06:20 +0300

    feat: added slime enemies, modified game for them to move
    They move from wall to wall, flip the sprite horizontally
    Kill player through killzone

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿### Notes
+
+File encoding is important: scenes need to use **UTF-8** without BOM, otherwise the engine might have trouble parsing them
---
+++ scripts/slime.gd
@@ -0,0 +1,23 @@
+﻿extends Node2D
+
+
+const SPEED = 60
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
+
+var direction = -1
+
+
+func _process(delta):
+	if (ray_cast_right.is_colliding()):
+		change_direction(-1)
+	elif (ray_cast_left.is_colliding()):
+		change_direction(1)
+
+	position.x += direction * SPEED * delta
+
+func change_direction(value):
+	direction = value
+	animated_sprite.flip_h = false if value == 1 else true
