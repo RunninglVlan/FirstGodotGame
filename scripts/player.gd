5e249d02f959884e31fba0d13444b9199e471388
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-12 00:01:12 +0300

    feat: added jump sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130
 const JUMP_VELOCITY = -300
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump_sound = $JumpSound
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,6 +19,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
