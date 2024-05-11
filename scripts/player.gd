eec17dbb0f188997bb9b4a0cec3b4e1f6bf15958
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-11 18:57:19 +0300

    feat: added input actions, more animations to player
    Flipping sprite based on direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 130
 const JUMP_VELOCITY = -300
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,15 +16,26 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	handle_animation(direction)
 
 	move_and_slide()
+
+
+func handle_animation(direction):
+	if (direction != 0):
+		animated_sprite.flip_h = false if direction > 0 else true
+	if is_on_floor():
+		animated_sprite.play("idle" if direction == 0 else "run")
+	else:
+		animated_sprite.play("jump")
