0c160a37d89e83aa3fd0f30fca875f47bfad9f31
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-13 00:27:29 +0300

    feat: using conditionals

--- gdscript-tutorial/gdscript-tutorial.gd
+++ gdscript-tutorial/gdscript-tutorial.gd
@@ -7,12 +7,28 @@ var health = 100
 
 
 func _ready():
-	label.text = str(health)
+	update_label()
 
 
 func _input(event):
 	if !(event is InputEventKey and event.pressed and event.keycode == KEY_SPACE):
 		return
 
-	health -= 20
-	label.text = str(health)
+	if health <= 0:
+		return
+
+	health = max(health - 20, 0)
+	update_label()
+	$HitSound.play()
+
+
+func update_label():
+	if health > 50:
+		label.text = "Healthy"
+		label.modulate = Color.GREEN
+	elif health > 0:
+		label.text = "Injured"
+		label.modulate = Color.ORANGE
+	else:
+		label.text = "Died"
+		label.modulate = Color.RED
