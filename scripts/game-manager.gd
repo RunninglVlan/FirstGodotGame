821b57a63524a8f4fd2dac2eeb8e53495b46813a
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-17 00:12:15 +0300

    feat: added button pressed handlers
    Autoloaded MultiplayerManager

--- scripts/game-manager.gd
+++ scripts/game-manager.gd
@@ -14,3 +14,13 @@ func add_point():
 	score += 1
 	score_label.text = str(score)
 	print(score)
+
+
+func host_game():
+	MultiplayerManager.host_game()
+	%MultiplayerHUD.hide()
+
+
+func join_game():
+	MultiplayerManager.join_game()
+	%MultiplayerHUD.hide()
---
+++ scripts/multiplayer/multiplayer-manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func host_game():
+	print("Hosting game")
+
+
+func join_game():
+	print("Joining game")
