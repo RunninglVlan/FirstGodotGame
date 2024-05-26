63668040384bb6addb4b7f118493df17fcd92644
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-26 14:16:10 +0300

    feat: spawning players
    On hosting/joining removing singleplayer
    Added MultiplayerSpawner which replicates nodes in the path
    Replicating host on client too

--- multiplayer/multiplayer_manager.gd
+++ multiplayer/multiplayer_manager.gd
@@ -4,10 +4,13 @@ extends Node
 const SERVER_ADDRESS = "127.0.0.1"
 const SERVER_PORT = 8080
 
+var player_spawn_path: Node
 var player_scene = load("res://multiplayer/multiplayer_player.tscn")
 
 
 func host_game():
+	player_spawn_path = get_tree().current_scene.get_node("Players")
+
 	var server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
 	multiplayer.multiplayer_peer = server_peer
@@ -15,12 +18,16 @@ func host_game():
 	multiplayer.peer_connected.connect(add_player_to_game)
 	multiplayer.peer_disconnected.connect(remove_player_from_game)
 
+	add_player_to_game(1)
+	remove_singleplayer()
+
 
 func add_player_to_game(id: int):
 	print("Player %s joined the game!" % id)
 	var player = player_scene.instantiate()
 	player.player_id = id
 	player.name = str(id)
+	player_spawn_path.add_child(player)
 
 
 func remove_player_from_game(id: int):
@@ -31,3 +38,10 @@ func join_game():
 	var client_peer = ENetMultiplayerPeer.new()
 	client_peer.create_client(SERVER_ADDRESS, SERVER_PORT)
 	multiplayer.multiplayer_peer = client_peer
+
+	remove_singleplayer()
+
+
+func remove_singleplayer():
+	var player = get_tree().current_scene.get_node("Player")
+	player.queue_free()
