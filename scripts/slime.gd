1e294b94478a8938d6077659fca0651c18b4ee54
RunninglVlan <RedCapShortCut@gmail.com>
2024-05-27 22:46:41 +0300

    feat: syncing slime positions and animations
    Movement is updated only on server
    Server sends animation changes to all
    - rpc calls work even without started multiplayer, at least local

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,4 +1,4 @@
-﻿extends Node2D
+extends Node2D
 
 
 const SPEED = 60
@@ -10,14 +10,25 @@ const SPEED = 60
 var direction = -1
 
 
+func _ready():
+	multiplayer.peer_connected.connect(stop_process)
+
+
+func stop_process(_id: int):
+	if not multiplayer.is_server():
+		set_process(false)
+
+
 func _process(delta):
 	if (ray_cast_right.is_colliding()):
-		change_direction(-1)
+		change_direction.rpc(-1)
 	elif (ray_cast_left.is_colliding()):
-		change_direction(1)
+		change_direction.rpc(1)
 
 	position.x += direction * SPEED * delta
 
+
+@rpc("call_local")
 func change_direction(value):
 	direction = value
 	animated_sprite.flip_h = false if value == 1 else true
